36e8e6f664e14d3ad7f451602d1a08a9975fb6fc
Abdellah Agourram <95725338+abdellah-agrm@users.noreply.github.com>
2024-07-29 11:03:47 +0100

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
 
   return (
     <motion.div id='home' variants={staggerContainer} initial="hidden" whileInView="show"
-      viewport={{ once: false, amount: 0.25 }} className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+      viewport={{ once: true, amount: 0.25 }} className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
 
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
         <TypingText title="Our Summer collections" textStyles="text-xl font-montserrat text-coral-red" />
